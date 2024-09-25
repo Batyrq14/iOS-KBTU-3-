4585c0d316f707530d8ed39a13c764fb4645000e
BatyrCode <utegenov1403@gmail.com>
2024-09-25 14:34:55 +0500

    done fibo

--- Task1/fibonacciSeq.swift
+++ Task1/fibonacciSeq.swift
@@ -1,8 +1,16 @@
-//
-//  fibonacciSeq.swift
-//  
-//
-//  Created by Batyrkhan Utegenov on 25.09.2024.
-//
-
 import Foundation
+func fibonacci(n: Int) {
+    var seq: [Int] = n == 0 ? [0] : [0, 1]
+    var currentNum = 2
+    while currentNum < n{
+        seq.append(seq[currentNum - 1] + seq[currentNum - 2])
+        currentNum += 1 }
+    print(seq)
+}
+
+print("Enter a number:")
+if let input = readLine(), let number = Int(input) {
+    print(fibonacci(n: number))
+} else {
+    print("Invalid input. Please enter a valid integer.")
+}
